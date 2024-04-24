5928f4afe941078d0e46a5b39e9371fe0c0cd01b
Sam Stoelinga <sammiestoel@gmail.com>
2024-04-24 09:51:37 -0700

    support variable node count

--- build-and-run.sh
+++ build-and-run.sh
@@ -6,6 +6,7 @@ export JOB_NAME=${JOB_NAME:-"stoelinga-nccl-test"}
 export PROJECT_ID=$(gcloud config get project)
 export IMAGE=gcr.io/${PROJECT_ID}/nccl-tests:$JOB_NAME
 export BUCKET_NAME=supercomputer-testing-stoelinga
+export NUM_NODES=${NUM_NODES:-4}
 
 docker build -t $IMAGE -f Dockerfile.ubuntu22 .
 docker push $IMAGE
